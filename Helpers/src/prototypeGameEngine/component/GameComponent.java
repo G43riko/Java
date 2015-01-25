8a467b71fb5b25894ee3057a956b7e947e42057c
G43riko <gcsollei@hotmail.com>
2015-01-25 11:23:53 +0100

    update GLib

---
+++ Helpers/src/prototypeGameEngine/component/GameComponent.java
@@ -0,0 +1,19 @@
+package prototypeGameEngine.component;
+
+public abstract class GameComponent {
+	public void render(){
+		
+	};
+	
+	public void update(){
+		
+	};
+	
+	public void init(){
+		
+	};
+	
+	public void input(){
+		
+	};
+}

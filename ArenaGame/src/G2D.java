fa94b0f6852781f25875748cad6ea3d96be11a3c
G43riko <gcsollei@hotmail.com>
2015-01-26 10:06:25 +0100

    fix formating

--- ArenaGame/src/G2D.java
+++ ArenaGame/src/G2D.java
@@ -1,21 +1,24 @@
-import java.awt.Color;
+import glib.util.GColor;
+import glib.util.noise.PerlinNoise;
+import glib.util.noise.SimplexNoise;
+
 import java.awt.Graphics;
 
 
 public class G2D {
 	private float[][] mapa;
 	G2D(){
-//		mapa = PerlinNoise.GeneratePerlinNoise(PerlinNoise.generateWhiteNoise(1600, 900), 8, 0.7f, true);
+		mapa = PerlinNoise.GeneratePerlinNoise(PerlinNoise.generateWhiteNoise(1600, 900), 8, 0.7f, true);
 		mapa = SimplexNoise.generateOctavedSimplexNoise(1600, 900, 6, 0.8f, 0.008f);
 	};
 	
 	public void drawMenu(Graphics g){
-		g.setColor(Color.DARK_GRAY);
+		g.setColor(GColor.DARK_GRAY);
 		g.fillRect(0,0,Main.WIDTH,Main.HEIGHT);
 	};
 	
 	public void drawGame(Graphics g){
-		g.setColor(Color.BLACK);
+		g.setColor(GColor.BLACK);
 		g.fillRect(0,0,Main.WIDTH,Main.HEIGHT);
 		
 		if(Main.gameIs==1){
@@ -24,17 +27,17 @@ public class G2D {
 					float color = mapa[i][j]*255;
 					color = Math.max(0,Math.min(255, color));
 					if(color < 51)
-						g.setColor(new Color(51,0,0,255));
+						g.setColor(new GColor(51,0,0,255));
 					else if(color < 102)
-						g.setColor(new Color(102,0,0,255));
+						g.setColor(new GColor(102,0,0,255));
 					else if(color < 153)
-						g.setColor(new Color(153,0,0,255));
+						g.setColor(new GColor(153,0,0,255));
 					else if(color < 204)
-						g.setColor(new Color(204,0,0,255));
+						g.setColor(new GColor(204,0,0,255));
 					else
-						g.setColor(new Color(255,0,0,255));
+						g.setColor(new GColor(255,0,0,255));
 					
-					g.setColor(new Color((int)color,(int)color,(int)color,255));
+					g.setColor(new GColor((int)color,(int)color,(int)color,255));
 					g.fillRect(i, j, 1, 1);		
 				}
 			}
